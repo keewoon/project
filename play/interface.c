9e1512f990bc06a66188785ed776ce5fcc8e0b85
keewoon bae <cong0_0@naver.com>
2016-11-24 23:14:28 -0800

    interface add3

--- play/interface.c
+++ play/interface.c
@@ -27,6 +27,29 @@ char options_f[100];
 char *pause_str[2]={"Pause\0","Resume\0"};
 char *start_stop_str[2]={"Start Game\0","Stop game\0"};
 gint timer;
+GtkWidget *score_label1;
+GtkWidget *score_label2;
+GtkWidget *level_label1;
+GtkWidget *level_label2;
+GtkWidget *lines_label1;
+GtkWidget *lines_label2;
+GtkWidget *menu_game_quick;
+GtkWidget *menu_game_start;
+GtkWidget *menu_game_stop;
+GtkWidget *menu_game_quit;
+GtkWidget *menu_game_pause;
+GtkWidget *menu_game_show_next_block;
+GtkWidget *menu_save_options;
+GtkWidget *spin_level;
+GtkWidget *spin_noise_level;
+GtkWidget *spin_noise_height;
+GtkWidget *new_game_window;
+GtkWidget *Start_stop_button;
+GtkWidget *Start_stop_button_label;
+GtkWidget *Pause_button;
+GtkWidget *Pause_button_label;
+GtkWidget *help_window;
+GtkWidget *about_window;
 
 
 
@@ -137,6 +160,110 @@ gboolean next_block_area_expose_event(GtkWidget *widget, GdkEventExpose *event, 
         return FALSE;
 }
 
+int game_loop()
+{
+	if(!game_over)
+	{
+		timer = gtk_timeout_add(level_speeds[current_level],
+					(GtkFunction)game_loop, NULL);
+		move_down();
+	}
+	return FALSE;
+}
+
+void game_set_pause_b()
+{
+  if(game_pause)
+    gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(menu_game_pause),FALSE);
+  else
+    gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(menu_game_pause),TRUE);
+  return;
+}
+
+void game_set_pause(GtkWidget    *menuitem,
+                    gpointer         user_data)
+{
+   if (game_over)
+    {
+      gtk_check_menu_item_set_active(GTK_CHECK_MENU_ITEM(menu_game_pause),
+                                     FALSE);
+      return;
+    }
+  game_pause = !game_pause;
+  if(game_pause) {
+    gtk_timeout_remove(timer);
+    gtk_label_set(GTK_LABEL(Pause_button_label),pause_str[1]);
+  }
+  else {
+    timer = gtk_timeout_add(level_speeds[current_level],
+                            (GtkFunction)game_loop,NULL);
+    gtk_label_set(GTK_LABEL(Pause_button_label),pause_str[0]);
+  }
+}
 
 
+void game_over_init()
+{
+        int high_dummy;
+        read_highscore();
+        if(current_score && (high_dummy = addto_highscore((char *)getenv("USER"),current_score,current_level,current_lines)))
+        {
+                write_highscore();
+                show_highscore(high_dummy);
+        }
+
+        game_over = TRUE;
+        game_play = FALSE;
+        gdk_draw_rectangle(game_area->window,
+                game_area->style->black_gc,
+                TRUE,
+                0,0,
+                game_area->allocation.width,
+                game_area->allocation.height);
+
+        gdk_draw_rectangle(next_block_area->window,
+                next_block_area->style->black_gc,
+                TRUE,
+                0,0,
+                next_block_area->allocation.width,
+                next_block_area->allocation.height);
+        game_set_pause(GTK_WIDGET(menu_game_pause),NULL);
+        gtk_label_set(GTK_LABEL(Start_stop_button_label),start_stop_str[0]);
+        gtk_widget_set_sensitive(menu_game_quick,TRUE);
+        gtk_widget_set_sensitive(menu_game_start,TRUE);
+        gtk_widget_set_sensitive(menu_game_stop,FALSE);
+        gtk_widget_set_sensitive(Start_stop_button,TRUE);
+        gtk_widget_grab_default(Start_stop_button);
+        gtk_label_set(GTK_LABEL(Pause_button_label),pause_str[0]);
+        gtk_widget_set_sensitive(Pause_button,FALSE);
+
+        gtk_timeout_remove(timer);
+}
+
+
+void game_start_stop(GtkMenuItem     *widget,
+                     gpointer user_data)
+{
+  game_play=!game_play;
+  gtk_widget_set_sensitive(GTK_WIDGET(widget), FALSE);
+  if(game_play)
+    {
+      gtk_widget_set_sensitive(menu_game_stop,TRUE);
+      gtk_widget_set_sensitive(menu_game_quick,FALSE);
+      gtk_widget_set_sensitive(menu_game_start,FALSE);
+      gtk_widget_set_sensitive(Start_stop_button,TRUE);
+      gtk_label_set(GTK_LABEL(Start_stop_button_label),start_stop_str[1]);
+      gtk_widget_set_sensitive(Pause_button,TRUE);
+      gtk_widget_grab_default(Pause_button);
+      game_init();
+      make_noise(options.noise_l,options.noise_h);
+      from_virtual();
+      move_block(0,0,0);
+      current_level = options.level;
+      update_game_values();
+      timer = gtk_timeout_add(level_speeds[current_level],(GtkFunction)game_loop,NULL);
+    }
+  else
+    game_over_init();
+}
 
