d9d35aa01c8d31b6d62d42e71d6500a68d81dd09
keewoon bae <cong0_0@naver.com>
2016-11-27 07:00:46 -0800

    add misc

--- function/misc.c
+++ function/misc.c
@@ -43,3 +43,37 @@ void set_gtk_color_style(GtkWidget *w, long red, long green,  long blue)
         gtk_widget_set_style(w,style);
         gtk_style_unref(style);
 }
+
+void get_opt_file(char *buf, int len)
+{
+  struct passwd *pw;
+
+  pw = getpwuid(getuid());
+  strcpy(buf,pw->pw_dir);
+  strcat(buf,"/.gtktetris\0");
+}
+
+
+GtkWidget *label_box (GtkWidget *parent, GtkWidget *label, gchar *label_text)
+{
+         GtkWidget *box1;
+         //GtkStyle *style;
+
+         /* create box for label */
+         box1 = gtk_hbox_new (FALSE, 0);
+         gtk_container_border_width (GTK_CONTAINER (box1), 2);
+
+         /* get style of button. */
+         //style = gtk_widget_get_style(parent);
+
+         /* create label for button */
+         gtk_label_set (GTK_LABEL(label), label_text);
+
+         /* pack the label into the box */
+
+         gtk_box_pack_start (GTK_BOX (box1), label, TRUE, TRUE, 3);
+         gtk_widget_show(label);
+
+         return (box1);
+}
+
